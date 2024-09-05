c03ff4e76244b0aa61f290797ab3b1cb72dd8477
Yun juyoung <yunju5090@gmail.com>
2024-09-05 19:52:00 +0900

    Toast, ImageView, EditText

--- test02/src/main/java/com/example/test02/MainActivity.kt
+++ test02/src/main/java/com/example/test02/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.test02
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,12 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_test03)
 
-        var btn03 = findViewById<Button>(R.id.btn03)
+        var edName = findViewById<EditText>(R.id.etName)
+        var edAge = findViewById<EditText>(R.id.etAge)
 
-        btn03.setOnClickListener{
-            btn03.visibility = View.INVISIBLE
+        var btnOK = findViewById<Button>(R.id.btnOK)
+
+        btnOK.setOnClickListener {
+            var name = edName.text
+            Toast.makeText(applicationContext, "입력한 이름 : " + name, Toast.LENGTH_SHORT).show()
         }
     }
 }
