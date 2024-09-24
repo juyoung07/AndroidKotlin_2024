ea969e5eb699b1ce9a5dfa61b896fc762a90bf17
Yun juyoung <yunju5090@gmail.com>
2024-09-24 19:58:41 +0900

    살짝 실습,,

--- ch07_layout/src/main/java/com/example/ch07_layout/MainActivity.kt
+++ ch07_layout/src/main/java/com/example/ch07_layout/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.ch07_layout
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.ch07_layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.btn1.setOnClickListener {
+            binding.tv1.visibility = View.VISIBLE;
+            binding.tv2.visibility = View.INVISIBLE;
+            binding.tv3.visibility= View.INVISIBLE;
         }
+        binding.btn2.setOnClickListener {
+            binding.tv2.visibility = View.VISIBLE;
+            binding.tv1.visibility = View.INVISIBLE;
+            binding.tv3.visibility= View.INVISIBLE;
+        }
+        binding.btn3.setOnClickListener {
+            binding.tv3.visibility = View.VISIBLE;
+            binding.tv1.visibility = View.INVISIBLE;
+            binding.tv2.visibility= View.INVISIBLE;
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
