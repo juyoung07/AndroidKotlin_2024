c1b6e599f4cf81ac60361885e5027bbafb254d86
Yun juyoung <yunju5090@gmail.com>
2024-10-22 19:47:42 +0900

    TableLayout.. main..

--- tabletest01/src/main/java/com/example/tabletest01/MainActivity.kt
+++ tabletest01/src/main/java/com/example/tabletest01/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.tabletest01
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.tabletest01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn.setOnClickListener {
+            var str = "이름: " + binding.edtName.text.toString() + "\n전화번호: " + binding.edtTel.text.toString()
+            Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
         }
     }
 }
